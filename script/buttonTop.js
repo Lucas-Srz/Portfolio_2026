3247ff548b7c0945adae38894cc4572522bd3698
Lucas-Srz <suarez.lucas.pro@gmail.com>
2026-04-10 10:41:52 +0200

    feat(all): Add button "go to top"

---
+++ script/buttonTop.js
@@ -0,0 +1,16 @@
+const btnTop = document.getElementById('buttonTop');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btnTop.style.display = "block";
+    } else {
+        btnTop.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
